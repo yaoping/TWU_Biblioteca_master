db7c0c9da02bdccf12e1608fff94b8d36a733d93
yaoping <ppyao@thoughtworks.com>
2015-09-15 23:10:02 +0800

    the funtion1

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,8 +1,9 @@
 package com.twu.biblioteca;
 
 public class BibliotecaApp {
+    public static String sendWelcomeMessage() {
+        return "Welcome";
 
-    public static void main(String[] args) {
-        System.out.println("Hello, world!");
     }
+
 }
---
+++ test/com/twu/biblioteca/BibliotecaAppTest.java
@@ -0,0 +1,15 @@
+package com.twu.biblioteca;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class BibliotecaAppTest {
+    @Test
+    public void should_show_welcome_message(){
+        assertThat(BibliotecaApp.sendWelcomeMessage(),is("Welcome"));
+    }
+
+
+}
